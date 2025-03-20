47dd515519284f719f7355cf2305b3474b176d94
AlgebraxCalculus <121043282+AlgebraxCalculus@users.noreply.github.com>
2025-03-20 08:44:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import NsxPage from "./pages/NsxPage";
+import SPPage from "./pages/SPPage";
+import PCPage from "./pages/PCPage";
+import LapTopPage from "./pages/LapTopPage";
+import ThongKeSPTheoLoai from "./pages/ThongKeSPTheoLoai";
+import ThongKeNSXTheoDC from "./pages/ThongKeNSXTheoDC";
+import Top5SPPage from "./pages/Top5SPPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/nsx" element={<NsxPage />} />
+        <Route path="/sanpham" element={<SPPage />} />
+        <Route path="/pc" element={<PCPage />} />
+        <Route path="/laptop" element={<LapTopPage />} />
+
+        {/* Không có trang ThongKeSPPage, các route con được đưa lên trực tiếp */}
+        <Route path="/sp/thongke" element={<ThongKeSPTheoLoai />} />
+        <Route path="/nsx/thongke" element={<ThongKeNSXTheoDC />} />
+        <Route path="/top-5/thongke" element={<Top5SPPage />} />
+      </Routes>
+    </Router>
   );
 }
 
